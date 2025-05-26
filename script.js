38fd00e2f3808a1d1a23ea9631da38f0334d0094
Bread <37907829+Wh1teBread@users.noreply.github.com>
2025-05-26 13:21:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function setLinks(){
 
         if (attr.includes("telegram")) text = "https://t.me/+iPfIDfzKqT84Y2Zi";
         if (attr.includes("youtube")) text = "https://www.youtube.com/channel/UCGo8qayYoA7kh9sccGNr-Gg";
-        if (attr.includes("instagram")) text = "https://t.me/+iPfIDfzKqT84Y2Zi";
+        if (attr.includes("instagram")) text = "https://www.instagram.com/wh1tebread3?igsh=ZDVocGwyNG92bzZ3";
         if (attr.includes("vk")) text = "https://vk.com/yahlep";
         
         link.addEventListener('click', function () {
