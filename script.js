828408e919809e20d4e2a64a430c7cd97b0205f5
Bread <37907829+Wh1teBread@users.noreply.github.com>
2025-06-20 09:07:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,15 @@ function setLinks(){
 
         if (attr.includes("telegram")) text = "https://t.me/+iPfIDfzKqT84Y2Zi";
         if (attr.includes("youtube")) text = "https://www.youtube.com/channel/UCGo8qayYoA7kh9sccGNr-Gg";
-        if (attr.includes("instagram")) text = "https://www.instagram.com/wh1tebread3?igsh=ZDVocGwyNG92bzZ3";
+        if (attr.includes("instagram")) text = "https://t.me/+iPfIDfzKqT84Y2Zi";
         if (attr.includes("vk")) text = "https://vk.com/yahlep";
+        if (attr.includes("lttrbxd")) text = "https://letterboxd.com/WatchBread/";
         
         link.addEventListener('click', function () {
             window.location.href = text;
         });
     }
 }
+
 setLinks();
+
